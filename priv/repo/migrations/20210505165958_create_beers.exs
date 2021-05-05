0ba58d806f11e1723ac7082a79d98a0fc4cb18ac
macmanu <manu@mac-manu.com>
2021-05-05 19:00:22 +0200

    schema migrations

---
+++ priv/repo/migrations/20210505165958_create_beers.exs
@@ -0,0 +1,15 @@
+defmodule BeerCatalog.Repo.Migrations.CreateBeers do
+  use Ecto.Migration
+
+  def change do
+    create table(:beers) do
+      add :brand, :string
+      add :style, :string
+      add :origin, :string
+      add :quantity, :integer
+
+      timestamps()
+    end
+
+  end
+end
